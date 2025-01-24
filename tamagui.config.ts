b8bcab78f649791eb61c63b3314b5a80afcc53a3
sephclt <josephajero22@gmail.com>
2025-01-24 18:44:33 +0800

    created custom theme

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,11 +1,44 @@
-import { tokens, themes, media, animations } from "@tamagui/config/v3";
-import { createTamagui } from "tamagui";
+import { tokens, media, animations } from "@tamagui/config/v3";
+import { createTamagui, createTokens } from "tamagui";
+
+const { color, ...restTokens } = tokens;
+
+const myTokens = createTokens({
+  color: {
+    white: "#ffffff",
+    lightGrey: "#F6EFEC",
+    grey: "#D8C6BD",
+    darkGrey: "#7C7876",
+    redOrange: "#CB806A",
+    darkRedOrange: "#4F3F39",
+    brown: "#9B8177",
+    black: "#443E3B",
+  },
+  ...restTokens,
+});
+
+const themes = {
+  light: {
+    background: myTokens.color.white,
+    subtleBackground: myTokens.color.lightGrey,
+    coloredBackground: myTokens.color.grey,
+    textColor: myTokens.color.black,
+    subtleTextColor: myTokens.color.darkGrey,
+  },
+  dark: {
+    background: myTokens.color.black,
+    subtleBackground: myTokens.color.darkRedOrange,
+    coloredBackground: myTokens.color.brown,
+    textColor: myTokens.color.white,
+    subtleTextColor: myTokens.color.darkGrey,
+  },
+};
 
 export const tamaguiConfig = createTamagui({
   tokens,
-  themes,
   media,
   animations,
+  themes,
 });
 
 export default tamaguiConfig;
