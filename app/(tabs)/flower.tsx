249a32301170e94367136c4a73edeac68f596bbe
sephclt <josephajero22@gmail.com>
2025-01-25 17:43:09 +0800

    added placeholder pages

---
+++ app/(tabs)/flower.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "tamagui";
+
+export default function Flower() {
+  return (
+    <View>
+      <Text>Welcome to Flower Page</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/user.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "tamagui";
+
+export default function User() {
+  return (
+    <View>
+      <Text>Welcome to User</Text>
+    </View>
+  );
+}
