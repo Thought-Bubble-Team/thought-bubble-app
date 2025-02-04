e32bdff4476a02876941ab7276d7c98cace94af2
sephclt <josephajero22@gmail.com>
2025-02-04 11:57:10 +0800

    feat: enhance Journals component with session management and journal entry display

--- app/(tabs)/journals.tsx
+++ app/(tabs)/journals.tsx
@@ -1,57 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { supabase } from "@/utils/supabase/supabase";
 import { Session } from "@supabase/supabase-js";
-import { styled, View, Text, XStack, Button } from "tamagui";
+import { styled, View, XStack, Button } from "tamagui";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 import MyView from "@/components/MyView";
 import MyScrollView from "@/components/MyScrollView";
 import MyText from "@/components/MyText";
-import MyCard from "@/components/MyCard";
+import { JournalCard, JournalEntryType } from "@/components/Cards";
+import { NoSession } from "@/components/Sessions";
 
-type Journal = {
-  id: number;
-  title: string;
-  content: string;
-  date: string;
+import { formatDate, splitFormattedDate } from "@/utils/dateFormat";
+
+const journalEntrySample: JournalEntryType = {
+  entry_id: 1,
+  title: "Today's Journal",
+  mood: "happy",
+  created_at: "2025-01-28 06:43:22.077857",
+  updated_at: "2025-01-28 06:43:22.077857",
+  content:
+    "Today was a good day. I had a lot of fun with my friends and family. I'm grateful for the time I spent with them.",
 };
-
-const date = new Date(); // Example date
-
-// Get day, date, and month
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-const day = days[date.getDay()]; // e.g., "Saturday"
-const dayOfMonth = date.getDate(); // e.g., 9
-const month = months[date.getMonth()]; // e.g., "Nov"
-
-const formattedDate = `, ${dayOfMonth} ${month}`;
 
 export default function Journals() {
   const [session, setSession] = useState<Session | null>(null);
-  const [journals, setJournals] = useState<Journal[]>([]);
+  const [journals, setJournals] = useState<JournalEntryType[]>([
+    journalEntrySample,
+  ]);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -69,30 +44,50 @@ export default function Journals() {
       paddingVertical={"$1"}
       backgroundColor={"$background"}
     >
-      <View
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        borderBottomWidth={"$1"}
-        borderBottomColor={"#EAE2DE"}
-        width={"100%"}
-        gap={"$4"}
-        padding={"$4"}
-      >
-        <MyText bold fontSize={30} color={"$textColor"}>
-          Your Journey
-        </MyText>
-      </View>
-      <MyScrollView width={"100%"} height={"100%"}>
-        <JournalEntry />
-      </MyScrollView>
+      {session && (
+        <View width={"100%"} height={"100%"}>
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderBottomWidth={"$1"}
+            borderBottomColor={"#EAE2DE"}
+            width={"100%"}
+            gap={"$4"}
+            padding={"$4"}
+          >
+            <MyText bold fontSize={30} color={"$textColor"}>
+              Your Journey
+            </MyText>
+          </View>
+          <MyScrollView width={"100%"} height={"100%"}>
+            {journals.map((journalEntrySample) => (
+              <JournalEntry
+                key={journalEntrySample.entry_id}
+                journalEntry={journalEntrySample}
+              />
+            ))}
+          </MyScrollView>
+        </View>
+      )}
+      {!session && <NoSession />}
     </MyView>
   );
 }
 
-const JournalEntry = () => {
+interface JournalEntryProps {
+  journalEntry: JournalEntryType;
+}
+
+const JournalEntry = (props: JournalEntryProps) => {
+  const { journalEntry } = props;
+
+  const formattedDate = formatDate(journalEntry.created_at);
+
+  const splitDate = splitFormattedDate(formattedDate);
+
   return (
-    <View display="flex" flexDirection="column">
+    <View display="flex" flexDirection="column" marginTop={"$4"} gap={0}>
       <View
         width={"100%"}
         paddingHorizontal={"$4"}
@@ -100,22 +95,21 @@ const JournalEntry = () => {
         flexDirection="row"
         justifyContent="space-between"
         alignItems="center"
+        margin={0}
       >
         <XStack>
           <MyText bold fontSize={20} color={"$textColor"}>
-            {day}
+            {splitDate[0]}
           </MyText>
           <MyText bold fontSize={20} color={"$subtleTextColor"}>
-            {formattedDate}
+            {splitDate[1]}
           </MyText>
         </XStack>
         <ButtonStyled>
-          <Ionicons name="settings-outline" size={24} color="#443E3B" />
+          <Ionicons name="settings-outline" size={18} color="#443E3B" />
         </ButtonStyled>
       </View>
-      <MyCard headerTitle="Today's Journal">
-        <MyText>Journal Entry</MyText>
-      </MyCard>
+      <JournalCard journalEntry={journalEntrySample}></JournalCard>
     </View>
   );
 };
