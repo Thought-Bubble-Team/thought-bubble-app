d7a8f0f6c20dbd4be4be87d0f34f1e7da959adf4
sephclt <josephajero22@gmail.com>
2025-02-04 09:32:19 +0800

    feat: add CRUD operations for journal entries using Supabase

---
+++ utils/supabase/db-crud.ts
@@ -0,0 +1,47 @@
+import { supabase } from "@/utils/supabase/supabase";
+
+export interface JournalEntry {
+  journal_title: string;
+  journal_content: string;
+}
+
+export const createJournalEntry = async (
+  journalEntry: Partial<JournalEntry>
+) => {
+  try {
+    const { data, error } = await supabase
+      .from("JournalEntry")
+      .insert([journalEntry]);
+
+    if (error) throw error;
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error };
+  }
+};
+
+export const getJournalEntries = async (user_id: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("JournalEntry")
+      .select("*")
+      .eq("user_id", user_id)
+      .order("created_at", { ascending: false });
+  } catch (error) {
+    return { data: null, error };
+  }
+};
+
+export const deleteJournalEntry = async (entryId: string) => {
+  try {
+    const { error } = await supabase
+      .from("JournalEntry")
+      .delete()
+      .eq("id", entryId);
+
+    if (error) throw error;
+    return { error: null };
+  } catch (error) {
+    return { error };
+  }
+};
