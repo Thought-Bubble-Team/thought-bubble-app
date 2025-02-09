7edd9cbc099df1740c63766450ae5be04e5d4ed4
sephclt <josephajero22@gmail.com>
2025-02-09 10:19:17 +0800

    feat: add Login and SignUp components for user authentication

---
+++ components/Macro/SignUp.tsx
@@ -0,0 +1,125 @@
+// Style Imports
+import React from "react";
+import { StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { styled, View } from "tamagui";
+
+// Component Imports
+import MyInput from "../Inputs/MyInput";
+import MyText from "../MyText";
+
+// Utility Imports
+import { useState } from "react";
+import { supabase } from "@/utils/supabase/supabase";
+
+// @ts-ignore
+import Logo from "../../assets/icons/logoTemp.svg";
+
+interface SignUpProps {
+  setIsSignUp: React.Dispatch<React.SetStateAction<boolean>>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function SignUp(props: SignUpProps) {
+  const { setIsSignUp, setLoading } = props;
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const signUpWithEmail = async () => {
+    setLoading(true);
+
+    if (password !== confirmPassword) {
+      Alert.alert("Error", "Passwords do not match!");
+      setLoading(false);
+      return;
+    }
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) {
+      Alert.alert("Error", error.message);
+    } else {
+      Alert.alert("Please check your inbox for email verification!");
+      setIsSignUp(false);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <MainContainer>
+      <Logo width={100} height={100} />
+      <MyText weight="bold" fontSize={18} textAlign="center">
+        Welcome to Thought Bubble!
+      </MyText>
+      <MyText weight="light" fontSize={13} textAlign="center">
+        Let your thoughts flow. Join us and start your journey today!
+      </MyText>
+      <MyInput
+        label="Email"
+        placeholder="johnydoe@gmail.com"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <MyInput
+        label="Password"
+        placeholder="********"
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+      <MyInput
+        label="Confirm Password"
+        placeholder="********"
+        secureTextEntry
+        onChangeText={setConfirmPassword}
+      />
+      <TouchableOpacity
+        style={buttonStyles.ButtonStyledColored}
+        onPress={signUpWithEmail}
+      >
+        <MyText weight="bold" fontSize={16} color={"$textColorAlt"}>
+          SIGNUP
+        </MyText>
+      </TouchableOpacity>
+      <Footer>
+        <MyText weight="light">Already have an account?</MyText>
+        <TouchableOpacity onPress={() => setIsSignUp(false)}>
+          <MyText weight="bold" color={"$accent"}>
+            Login
+          </MyText>
+        </TouchableOpacity>
+      </Footer>
+    </MainContainer>
+  );
+}
+
+// Tamagui Styles
+const MainContainer = styled(View, {
+  width: "100%",
+  gap: "$6",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const Footer = styled(View, {
+  flexDirection: "row",
+  gap: "$2",
+  alignItems: "center",
+  marginTop: "$10",
+});
+
+// React Native Styles
+const buttonStyles = StyleSheet.create({
+  ButtonStyledColored: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    padding: 16,
+    backgroundColor: "#CB806A",
+    color: "#fff",
+    borderRadius: 32,
+  },
+});
