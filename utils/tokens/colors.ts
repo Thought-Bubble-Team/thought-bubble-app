40c76ac762f45f8ce8a7eb50cd9ed737c0b8b69a
sephclt <josephajero22@gmail.com>
2025-02-13 21:57:25 +0800

    feat: add color and theme utility files for consistent styling

---
+++ utils/tokens/colors.ts
@@ -0,0 +1,38 @@
+export enum Colors {
+    white= "#ffffff",
+    lightGrey= "#F6EFEC",
+    grey= "#D8C6BD",
+    darkGrey= "#7C7876",
+    redOrange= "#CB806A",
+    darkRedOrange= "#4F3F39",
+    brown= "#9B8177",
+    black= "#443E3B",
+
+    red= "#C95F4A",
+    darkRed= "#B34E3A",
+    softRed= "#D87A64",
+
+    orange= "#D28A5E",
+    darkOrange= "#BF7448",
+    softOrange= "#E19C72",
+
+    yellow= "#D9B27C",
+    darkYellow= "#C29C67",
+    softYellow= "#E4C18E",
+
+    green= "#8F9775",
+    darkGreen= "#7A8363",
+    softGreen= "#A5AD89",
+
+    blue= "#6E7B8F",
+    darkBlue= "#5B687B",
+    softBlue= "#8793A5",
+
+    purple= "#8A6D85",
+    darkPurple= "#745770",
+    softPurple= "#9F819A",
+
+    pink= "#C78C8F",
+    darkPink= "#B3797C",
+    softPink= "#D39FA2",
+}
