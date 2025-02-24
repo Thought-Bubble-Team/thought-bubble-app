972b019f9fc8bf552c7785334cddeb0634fe431e
sephclt <josephajero22@gmail.com>
2025-02-24 23:28:46 +0800

    refactor: update Tamagui tokens to enhance size and color configurations for improved theming

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,46 +1,28 @@
 import { tokens, media, animations } from "@tamagui/config/v3";
 import { createTamagui, createTokens } from "tamagui";
 
-const { color, ...restTokens } = tokens;
+const { size, space, ...restTokens } = tokens;
 
 const myTokens = createTokens({
-  color: {
-    white: "#ffffff",
-    lightGrey: "#F6EFEC",
-    grey: "#D8C6BD",
-    darkGrey: "#7C7876",
-    redOrange: "#CB806A",
-    darkRedOrange: "#4F3F39",
-    brown: "#9B8177",
-    black: "#443E3B",
-
-    red: "#C95F4A",
-    darkRed: "#B34E3A",
-    softRed: "#D87A64",
-
-    orange: "#D28A5E",
-    darkOrange: "#BF7448",
-    softOrange: "#E19C72",
-
-    yellow: "#D9B27C",
-    darkYellow: "#C29C67",
-    softYellow: "#E4C18E",
-
-    green: "#8F9775",
-    darkGreen: "#7A8363",
-    softGreen: "#A5AD89",
-
-    blue: "#6E7B8F",
-    darkBlue: "#5B687B",
-    softBlue: "#8793A5",
-
-    purple: "#8A6D85",
-    darkPurple: "#745770",
-    softPurple: "#9F819A",
-
-    pink: "#C78C8F",
-    darkPink: "#B3797C",
-    softPink: "#D39FA2",
+  size: {
+    ...size,
+    xs: 8,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 20,
+    xxl: 24,
+    xxxl: 30,
+  },
+  space: {
+    ...space,
+    xs: 8,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 20,
+    xxl: 24,
+    xxxl: 30,
   },
   ...restTokens,
 });
@@ -67,11 +49,24 @@ const themes = {
     divider: "#BBADA6",
   },
   dark: {
-    background: myTokens.color.darkRedOrange,
-    subtleBackground: myTokens.color.darkRedOrange,
-    coloredBackground: myTokens.color.brown,
-    textColor: myTokens.color.white,
-    subtleTextColor: myTokens.color.darkGrey,
+    primary: "#FF5733",
+    secondary: "#33A1FF",
+    background: "#443E3B",
+    white: "#E9D9D0",
+    black: "#000000",
+    grey0: "#90766F",
+    grey1: "#BCA49D",
+    grey2: "#BBADA6",
+    grey3: "#D8C6BD",
+    grey4: "#E9D9D0",
+    grey5: "#F6EFEC",
+    greyOutline: "#90766F",
+    searchBg: "#443E3B",
+    success: "#4CAF50",
+    warning: "#FFC107",
+    error: "#F44336",
+    disabled: "#BCA49D",
+    divider: "#90766F",
   },
 };
 
