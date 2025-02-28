3e88cdadcbc97f5f5434e3bd35f678ddca93475f
sephclt <josephajero22@gmail.com>
2025-02-28 12:13:50 +0800

    feat: add new components for MoodBar, MoodCalendar, MoodFlow, ReoccurringWords, Appearance, EditProfile, MyData, and Preferences

---
+++ app/user/[id]/edit-profile.tsx
@@ -0,0 +1,17 @@
+import { View } from "tamagui";
+import Text from "@/components/Micro/Text";
+
+import { useLocalSearchParams, Stack } from "expo-router";
+
+const EditProfileScreen = () => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Stack.Screen options={{ headerTitle: "Edit Profile" }} />
+      <Text>{id}</Text>
+      <Text>Edit Profile</Text>
+    </View>
+  );
+};
+
+export default EditProfileScreen;
---
+++ app/user/[id]/my-data.tsx
@@ -0,0 +1,17 @@
+import { View } from "tamagui";
+import Text from "@/components/Micro/Text";
+
+import { useLocalSearchParams, Stack } from "expo-router";
+
+const MyData = () => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Stack.Screen options={{ headerTitle: "My Data" }} />
+      <Text>{id}</Text>
+      <Text>My Data</Text>
+    </View>
+  );
+};
+
+export default MyData;
