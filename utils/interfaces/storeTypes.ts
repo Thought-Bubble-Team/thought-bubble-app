de2e3632bbcf7cbc313bcd0d1c4f60d8f7d105ee
sephclt <josephajero22@gmail.com>
2025-03-05 19:26:50 +0800

    chore: added gratitude entries store type

--- utils/interfaces/storeTypes.ts
+++ utils/interfaces/storeTypes.ts
@@ -6,3 +6,10 @@ export type JournalEntriesStoreType = {
   error: any;
   fetchJournalEntries: () => Promise<void>;
 };
+
+export type GratitudeEntriesStoreType = {
+  gratitude_entries: JournalEntriesType | null;
+  loading: boolean;
+  error: any;
+  fetchGratitudeEntries: () => Promise<void>;
+};
