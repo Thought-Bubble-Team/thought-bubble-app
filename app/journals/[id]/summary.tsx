27d97b9161be4529ecc1a2b53d98b56c5644e307
sephclt <josephajero22@gmail.com>
2025-03-06 01:02:04 +0800

    feat: implement initial layout for Journal Summary page with navigation and form

--- app/journals/[id]/summary.tsx
+++ app/journals/[id]/summary.tsx
@@ -1,15 +1,25 @@
 import { View } from "tamagui";
-import Text from "@/components/atoms/Text";
-
 import { useLocalSearchParams, Stack } from "expo-router";
 
+import Text from "@/components/atoms/Text";
+import Navigation from "@/components/atoms/Navigation";
+import JournalForm from "@/components/Macro/JournalForm";
+
+// TODO: Implement the Journal Summary page
+// TODO: Add a button to edit the journal entry
+// TODO: Add a section to display the journal entry's images
+// TODO: Make the title of the page dynamic based on the journal entry's creation date
 const Summary = () => {
   const { id } = useLocalSearchParams();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}
+    >
+      <Navigation title="Journal Summary" />
       <Stack.Screen options={{ title: "Summary" }} />
-      <Text>{id}</Text>
-      <Text>Summary</Text>
+      <View flex={1} justifyContent={"center"} alignItems={"center"}>
+        <JournalForm />
+      </View>
     </View>
   );
 };
