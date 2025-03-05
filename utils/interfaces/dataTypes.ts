bc508507aab3deb34781eaa2fb9f3e83f907edb8
sephclt <josephajero22@gmail.com>
2025-03-05 10:53:35 +0800

    feat: add data type for journal entry and journal entries

--- utils/interfaces/dataTypes.ts
+++ utils/interfaces/dataTypes.ts
@@ -6,3 +6,14 @@ export type MoodCalendarDataType = {
     sentiment: string;
   }[];
 };
+
+export type JournalEntryType = {
+  entry_id: number;
+  user_id: string;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type JournalEntriesType = JournalEntryType[];
