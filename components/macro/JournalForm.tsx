d4713c1a7bf62b912b976864768f23c5ddac4c86
sephclt <josephajero22@gmail.com>
2025-03-08 17:53:27 +0800

    fix: fixed image container not shrinking after removing all images

--- components/macro/JournalForm.tsx
+++ components/macro/JournalForm.tsx
@@ -23,7 +23,7 @@ import {
 } from "@/utils/supabase/db-crud";
 
 // TODO: Make image pressable to view full screen
-// REF: update handleSubmit to handle errors properly
+// TODO: update handleSubmit to handle errors properly
 
 export default function JournalForm({ editable = true }: JournalFormProps) {
   const theme = useTheme();
@@ -269,7 +269,7 @@ export default function JournalForm({ editable = true }: JournalFormProps) {
       )}
 
       {/* Images */}
-      {images !== undefined && (
+      {images !== undefined && images.length > 0 && (
         <ScrollView horizontal backgroundColor={"$grey0"} maxHeight={100}>
           {images.map((image, index) => (
             <ImageWrapper key={index} style={{ zIndex: images.length - index }}>
@@ -344,7 +344,7 @@ const RemoveImageWrapper = styled(View, {
   borderRadius: 32,
   borderWidth: 2,
   borderColor: "$grey0",
-  backgroundColor: "$grey2",
+  backgroundColor: "$grey3",
   justifyContent: "center",
   alignItems: "center",
   zIndex: 10,
