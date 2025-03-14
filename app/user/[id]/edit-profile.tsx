7622bc4523ed118f98d86c3767a97b0d484653d1
sephclt <josephajero22@gmail.com>
2025-03-14 19:12:48 +0800

    refactor: remove unused imports and parameters from EditProfileScreen

--- app/user/[id]/edit-profile.tsx
+++ app/user/[id]/edit-profile.tsx
@@ -1,12 +1,8 @@
 import Screen from "@/components/atoms/Screen";
-import Text from "@/components/atoms/Text";
 import EditProfile from "@/components/macro/EditProfile";
 import { Navigation } from "@/components/macro/Navigation";
 
-import { useLocalSearchParams, Stack } from "expo-router";
-
 const EditProfileScreen = () => {
-  const { id } = useLocalSearchParams();
   return (
     <Screen testID="edit-profile-screen" justifyContent="flex-start">
       <Navigation title="Edit Profile" />
